b9499cd01d9337ec2190e7998213d794e088e817
Adama55 <mafahfof55@gmail.com>
2024-05-09 19:19:56 +0200

    first

---
+++ database/firebse.py
@@ -0,0 +1,24 @@
+# Importe le module firebase_admin nécessaire pour l'initialisation de Firebase
+import firebase_admin
+
+from firebase_admin import credentials
+
+import pyrebase
+
+from configs.firebase_config import firebase_config
+
+if not firebase_admin._apps:
+
+    # Charge les informations d'authentification
+    cred = credentials.Certificate("configs/config_key.json")
+
+    # Initialise l'application Firebase 
+    firebase_admin.initialize_app(cred)
+
+# Initialise l'application Firebase
+firebase = pyrebase.initialize_app(firebase_config)
+
+# Crée une instance de la base de données Firebase
+db = firebase.database()
+authStudent = firebase.auth()
+authUser = firebase.auth()
